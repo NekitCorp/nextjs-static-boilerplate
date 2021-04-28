1409203dc50681a0a946462ed142b39ffa9411a0
Nikita <madeev@itglobal.ru>
2021-04-28 20:42:37 +0300

    Init project

---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['stylelint-config-standard', 'stylelint-config-idiomatic-order'],
+    rules: {
+        indentation: 4,
+    },
+    ignoreFiles: ['**/*.{tsx,ts,js}'],
+};
